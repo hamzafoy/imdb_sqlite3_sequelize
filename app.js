cb2f840e0171babf430dff0e6d5100b719a66d06
Ross Hamza Foy <hamzaoflouisville@gmail.com>
2021-02-26 13:20:45 -0500

    update the app to include a sequelize model for movies and a sqlite database for movies.

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ const sequelize = new Sequelize({
   storage: 'movies.db'
 });
 
+//Movie Model
+class Movie extends Sequelize.Model {}
+Movie.init({
+  title: Sequelize.STRING
+}, { sequelize });
+
 (async () => {
+    await sequelize.sync({ force: true });
+
     try {
       await sequelize.authenticate();
       console.log('Connection to the database successful!');
+      const movie = await Movie.create({ title: 'Toy Story' });
+      console.log(movie.toJSON());
     } catch (error) {
       console.error('Error connecting to the database: ', error);
     }
