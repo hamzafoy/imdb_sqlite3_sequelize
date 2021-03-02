3952ea2c85f199409dca6a2c0aea494a36a38510
Ross Hamza Foy <hamzaoflouisville@gmail.com>
2021-03-01 19:10:36 -0500

    add validators to the Movie model.

--- db/models/movie.js
+++ db/models/movie.js
@@ -13,14 +13,44 @@ module.exports = (sequelize) => {
         title: { 
             type: Sequelize.STRING(500),
             allowNull: false,
+            validate: {
+                notEmpty: {
+                    //customized error message below
+                    msg: 'You must provide a value for "title"',
+                },
+                notNull: {
+                    //customized error message below
+                    msg: 'You cannot leave the "title" empty!'
+                }
+            },
          },
         runtime: { 
             type: Sequelize.INTEGER,
             allowNull: false,
+            validate: {
+                notNull: {
+                    //customized error message below
+                    msg: 'You cannot leave the "runtime" empty!'
+                },
+                min: {
+                    args: 1,
+                    msg: 'You must provide a value greater than 0 with "runtime"',
+                },
+                max: {
+                    args: 500,
+                    msg: 'No movie runs that long, you must provide a value less than 499',
+                }
+            },
          },
         releaseDate: { 
             type: Sequelize.DATEONLY,
             allowNull: false,
+            validate: {
+                notNull: {
+                    //customized error message below
+                    msg: 'You cannot leave the "releaseDate" empty!'
+                }
+            },
          },
         isAvailableOnVHS: { 
             type: Sequelize.BOOLEAN,
